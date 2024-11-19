66c906a40fcf3077550a54a0661859ab9d8740d5
Moneer Ashraf <moneerashraf824@gmail.com>
2024-11-19 21:24:45 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -6,24 +6,24 @@ import axios from "axios";
 const API_URL_v2 = process.env.REACT_APP_API_URL_V2;
 
 // Users api
-export const loginUser = (user) => axios.post(`${API_URL_v2}/users/login`, user);
-export const signupUser = (user) => axios.post(`${API_URL_v2}/users/signup`, user);
+export const loginUser = (user) => axios.post(`${API_URL_v2}/users/login`, user , { withCredentials: true });
+export const signupUser = (user) => axios.post(`${API_URL_v2}/users/signup`, user );
 export const getUsers = () => axios.get(`${API_URL_v2}/users`);
 export const getUserById = (id) => axios.get(`${API_URL_v2}/users/${id}`);
 
 // Tasks api
-export const getTaskByUserId = (userId) => axios.get(`${API_URL_v2}/tasks?created_by=${userId}`);
-export const createTask = (task) => axios.post(`${API_URL_v2}/tasks`, task);
-export const updateTask = (id, updates) => axios.patch(`${API_URL_v2}/tasks/${id}`, updates);
-export const deleteTask = (id) => axios.delete(`${API_URL_v2}/tasks/${id}`);
+export const getTaskByUserId = (userId) => axios.get(`${API_URL_v2}/tasks?created_by=${userId}` , { withCredentials: true });
+export const createTask = (task) => axios.post(`${API_URL_v2}/tasks`, task , { withCredentials: true });
+export const updateTask = (id, updates) => axios.patch(`${API_URL_v2}/tasks/${id}`, updates, { withCredentials: true });
+export const deleteTask = (id) => axios.delete(`${API_URL_v2}/tasks/${id}` , { withCredentials: true });
 
 // Projects api
-export const createProject = (project) => axios.post(`${API_URL_v2}/projects`, project);
-export const getProjects = (userId) => axios.get(`${API_URL_v2}/projects?user_id=${userId}`);
-export const updateProject = (id, updates) => axios.patch(`${API_URL_v2}/projects/${id}`, updates);
-export const deleteProject = (id) => axios.delete(`${API_URL_v2}/projects/${id}`);
+export const createProject = (project) => axios.post(`${API_URL_v2}/projects`, project, { withCredentials: true });
+export const getProjects = (userId) => axios.get(`${API_URL_v2}/projects?user_id=${userId}`, { withCredentials: true });
+export const updateProject = (id, updates) => axios.patch(`${API_URL_v2}/projects/${id}`, updates, { withCredentials: true });
+export const deleteProject = (id) => axios.delete(`${API_URL_v2}/projects/${id}`, { withCredentials: true });
 
 // Teams api
-export const createTeam = (team) => axios.post(`${API_URL_v2}/teams`, team);
-export const getTeam = (user_id) => axios.get(`${API_URL_v2}/teams?user_id=${user_id}`);
+export const createTeam = (team) => axios.post(`${API_URL_v2}/teams`, team, { withCredentials: true });
+export const getTeam = (user_id) => axios.get(`${API_URL_v2}/teams?user_id=${user_id}`, { withCredentials: true });
 
