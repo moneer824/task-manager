b1072f1546d48a7f61eb9ea339faf2f7b2a45b75
Moneer Ashraf <moneerashraf824@gmail.com>
2025-01-14 17:01:15 +0530

    Update BoardView.jsx

--- src/components/BoardView.jsx
+++ src/components/BoardView.jsx
@@ -9,7 +9,7 @@ import NoContent from "./NoContent";
 import { TEMPLATE_NAME } from "../services/constant";
 
 function BoardView() {
-    const { tasks , setTasks, task_status_constants, updateSelectedTask, activeTemplate, setActiveTemplate } = useAuth();
+    const { tasks, setTasks, task_status_constants, updateSelectedTask, activeTemplate, setActiveTemplate } = useAuth();
     const [taskType, setTaskType] = useState('add');
     const [editTaskData, setEditTaskData] = useState(null);
     const [isOpen, setIsOpen] = useState(false);
@@ -23,12 +23,12 @@ function BoardView() {
 
         let task_id = null;
         if (source.droppableId !== destination.droppableId) {
-        // Update the task's status to match the new column
+            // Update the task's status to match the new column
             const updatedTasks = tasksData.map((task) => {
                 if (task._id === result.draggableId) {
                     task_id = task._id
-                    console.log('task_id',task._id , destination.droppableId)
-                return { ...task, status: destination.droppableId };
+                    console.log('task_id', task._id, destination.droppableId)
+                    return { ...task, status: destination.droppableId };
                 }
                 return task;
             });
@@ -42,7 +42,7 @@ function BoardView() {
     useEffect(() => {
         if (project_id == 'all') {
             setTasksData(tasks);
-        }else{
+        } else {
             let filteredTasks = tasks.filter((task) => task.project_id === project_id);
             setTasksData(filteredTasks);
         }
@@ -52,53 +52,53 @@ function BoardView() {
         <div className="kanban-board-container">
             <div className="create-task-modal">
                 <h3>Tasks</h3>
-                <CreateTasksModal setTaskType={setTaskType} taskType={taskType} editTaskData = {editTaskData} isOpen={isOpen} toggle={toggle} />
+                <CreateTasksModal setTaskType={setTaskType} taskType={taskType} editTaskData={editTaskData} isOpen={isOpen} toggle={toggle} />
             </div>
             <div className="abcd">
-            {tasks.length > 0 && tasksData.length > 0 && <DragDropContext onDragEnd={handleDragEnd}>
-                <div className="kanban-board">
-                    {Object.entries(task_status_constants).map(([status, title]) => (
-                        <Droppable key={status} droppableId={status}>
-                        {(provided) => (
-                            <div
-                            className="kanban-column"
-                            ref={provided.innerRef}
-                            {...provided.droppableProps}
-                            >
-                                <div className="kanban-column-details">
-                                    <h2 className="kanban-column-title">{title} </h2>
-                                    <span className="kanban-column-info">{tasksData.filter((task) => task.status === status).length}</span>
-                                </div>
-                            <div className="kanban-tasks">
-                                {tasksData
-                                .filter((task) => task.status === status)
-                                .map((task, index) => (
-                                    <Draggable
-                                    key={task._id}
-                                    draggableId={task._id}
-                                    index={index}
+                {tasks.length > 0 && tasksData.length > 0 && <DragDropContext onDragEnd={handleDragEnd}>
+                    <div className="kanban-board">
+                        {Object.entries(task_status_constants).map(([status, title]) => (
+                            <Droppable key={status} droppableId={status}>
+                                {(provided) => (
+                                    <div
+                                        className="kanban-column"
+                                        ref={provided.innerRef}
+                                        {...provided.droppableProps}
                                     >
-                                    {(provided) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                        >
-                                            <BoardTask task={task} setEditTaskData = {setEditTaskData} setTaskType = {setTaskType} toggle={toggle}  />
+                                        <div className="kanban-column-details">
+                                            <h2 className="kanban-column-title">{title} </h2>
+                                            <span className="kanban-column-info">{tasksData.filter((task) => task.status === status).length}</span>
                                         </div>
-                                    )}
-                                    </Draggable>
-                                ))}
-                                {provided.placeholder}
-                            </div>
-                            </div>
-                        )}
-                        </Droppable>
-                    ))}
-                </div>
-            </DragDropContext>}
-            {tasks.length === 0 && <NoContent title="Task" info="You do not have any Task" toggle={toggle} />}
-            {tasks.length > 0 && tasksData.length === 0 && <NoContent title="Task" info="You have Task but they are not assigned to this project." toggle={toggle} />}
+                                        <div className="kanban-tasks">
+                                            {tasksData
+                                                .filter((task) => task.status === status)
+                                                .map((task, index) => (
+                                                    <Draggable
+                                                        key={task._id}
+                                                        draggableId={task._id}
+                                                        index={index}
+                                                    >
+                                                        {(provided) => (
+                                                            <div
+                                                                ref={provided.innerRef}
+                                                                {...provided.draggableProps}
+                                                                {...provided.dragHandleProps}
+                                                            >
+                                                                <BoardTask task={task} setEditTaskData={setEditTaskData} setTaskType={setTaskType} toggle={toggle} />
+                                                            </div>
+                                                        )}
+                                                    </Draggable>
+                                                ))}
+                                            {provided.placeholder}
+                                        </div>
+                                    </div>
+                                )}
+                            </Droppable>
+                        ))}
+                    </div>
+                </DragDropContext>}
+                {tasks.length === 0 && <NoContent title="Task" info="You do not have any Task" toggle={toggle} />}
+                {tasks.length > 0 && tasksData.length === 0 && <NoContent title="Task" info="You have Task but they are not assigned to this project." toggle={toggle} />}
             </div>
         </div>
     );
