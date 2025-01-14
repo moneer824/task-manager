043f2fd0b063209121b20123daebcfbb6e1c32e5
Moneer Ashraf <moneerashraf824@gmail.com>
2025-01-14 16:55:04 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import logo from './logo.svg';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal, ModalBody, ModalHeader, Button  } from 'reactstrap';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -10,9 +6,10 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import SideNavbar from './components/SideNavbar';
 import Tasks from './pages/Tasks';
-import GroupMembers from './pages/GroupMembers';
 import DashBoard from './pages/DashBoard';
 import ViewTeams from './pages/ViewTeams';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
